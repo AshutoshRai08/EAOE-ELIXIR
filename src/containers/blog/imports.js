8bbbabbd3e7e1281329825e656234ac9de99ad0f
Ashutosh Rai <rai.ashu0tosh11@gmail.com>
2023-04-06 10:38:51 +0530

    blogjs

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,235 @@
+import img1 from '../../assets/BlogImages/1.png';
+import img2 from '../../assets/BlogImages/2.png';
+import img3 from '../../assets/BlogImages/3.png';
+import img4 from '../../assets/BlogImages/4.png';
+import img5 from '../../assets/BlogImages/5.png';
+import img6 from '../../assets/BlogImages/6.png';
+
+const data = [
+  {
+      id: 1,
+      img: img1,
+      name: 'Esther Howard',
+      position: 'Co-founder & COO',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 2,
+      img: img2,
+      name: 'Devon Lane',
+      position: 'Blockchain Developer',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 3,
+      img: img3,
+      name: 'Ralph Edwards',
+      position: 'Backend Engineer',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 4,
+      img: img4,
+      name: 'Esther Howard',
+      position: 'Co-founder & COO',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 5,
+      img: img5,
+      name: 'Devon Lane',
+      position: 'Blockchain Developer',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 6,
+      img: img6,
+      name: 'Ralph Edwards',
+      position: 'Backend Engineer',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 7,
+      img: img1,
+      name: 'Esther Howard',
+      position: 'Co-founder & COO',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+  {
+      id: 8,
+      img: img2,
+      name: 'Devon Lane',
+      position: 'Blockchain Developer',
+      listsocial: [
+          {
+              id: 1,
+              icon: 'fab fa-twitter',
+              link: '#'
+          },
+          {
+              id: 2,
+              icon: 'fab fa-facebook-f',
+              link: '#'
+          },
+          {
+              id: 3,
+              icon: 'fa fa-paper-plane',
+              link: '#'
+          },
+          {
+              id: 4,
+              icon: 'fab fa-reddit',
+              link: '#'
+          },
+      ]
+  },
+]
+
+export default data;
