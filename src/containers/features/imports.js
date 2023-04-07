cd60830d44550dd6f00a7f067503f389f47596ae
Ashutosh Rai <rai.ashu0tosh11@gmail.com>
2023-04-07 21:24:15 +0530

    contain_features

---
+++ src/containers/features/imports.js
@@ -0,0 +1,140 @@
+import img1 from '../../assets/BlogImages/1.png'
+import img2 from '../../assets/BlogImages/2.png'
+import img3 from '../../assets/BlogImages/3.png'
+import img4 from '../../assets/BlogImages/4.png'
+import img5 from '../../assets/BlogImages/5.png'
+import img6 from '../../assets/BlogImages/6.png'
+import img7 from '../../assets/BlogImages/7.png'
+import img9 from '../../assets/BlogImages/9.png'
+import img10 from '../../assets/BlogImages/10.png'
+import img11 from '../../assets/BlogImages/11.png'
+import img12 from '../../assets/BlogImages/12.png'
+import img13 from '../../assets/BlogImages/13.png'
+import img14 from '../../assets/BlogImages/14.png'
+
+
+const data = [
+    {
+        id: 1,
+        title: 'ELIXER #01',
+        img: img1,
+    },
+    {
+        id: 2,
+        title: 'ELIXER #02',
+        img: img2,
+    },
+    {
+        id: 3,
+        title: 'ELIXER #03',
+        img: img3,
+    },
+    {
+        id: 4,
+        title: 'ELIXER #04',
+        img: img4,
+    },
+    {
+        id: 5,
+        title: 'ELIXER #05',
+        img: img5,
+    },
+    {
+        id: 6,
+        title: 'ELIXER #06',
+        img: img6,
+    },
+    {
+        id: 7,
+        title: 'ELIXER #07',
+        img: img7,
+    },
+    {
+        id: 8,
+        title: 'ELIXER #08',
+        img: img2,
+    },
+
+    {
+        id: 9,
+        title: 'ELIXER #09',
+        img: img9,
+    },
+    {
+        id: 10,
+        title: 'ELIXER #10',
+        img: img10,
+    },
+    {
+        id: 11,
+        title: 'ELIXER #11',
+        img: img11,
+    },
+    {
+        id: 12,
+        title: 'ELIXER #12',
+        img: img12,
+    },
+    {
+        id: 13,
+        title: 'ELIXER #13',
+        img: img13,
+    },
+    {
+        id: 14,
+        title: 'ELIXER #14',
+        img: img14,
+    },
+    {
+        id: 15,
+        title: 'ELIXER #15',
+        img: img1,
+    },
+    {
+        id: 16,
+        title: 'ELIXER #16',
+        img: img3,
+    },
+    {
+        id: 17,
+        title: 'ELIXER #01',
+        img: img1,
+    },
+    {
+        id: 18,
+        title: 'ELIXER #02',
+        img: img2,
+    },
+    {
+        id: 19,
+        title: 'ELIXER #03',
+        img: img3,
+    },
+    {
+        id: 20,
+        title: 'ELIXER #04',
+        img: img4,
+    },
+    {
+        id: 21,
+        title: 'ELIXER #05',
+        img: img5,
+    },
+    {
+        id: 22,
+        title: 'ELIXER #06',
+        img: img6,
+    },
+    {
+        id: 23,
+        title: 'ELIXER #07',
+        img: img7,
+    },
+    {
+        id: 24,
+        title: 'ELIXER #08',
+        img: img2,
+    },
+]
+
+export default data;
