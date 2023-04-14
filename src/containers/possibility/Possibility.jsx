c94c578dad0544bf03e3e4800429474a67d7fcf0
Ashutosh Rai <rai.ashu0tosh11@gmail.com>
2023-04-14 20:55:56 +0530

    possibility

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './possibility.scss';
+
+const Possibility = () => (
+  <section className="road-map" id="possibility">
+
+  </section>
+);
+
+export default Possibility;
